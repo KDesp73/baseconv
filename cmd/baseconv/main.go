c84ce909cb7843ee5fd15e830242f02abffb722c
Konstantinos Despoinidis <kdesp2003@gmail.com>
2024-09-13 01:58:54 +0300

    using flag to enable debug logging

--- cmd/baseconv/main.go
+++ cmd/baseconv/main.go
@@ -2,17 +2,25 @@ package main
 
 import (
 	"baseconv/cmd/baseconv/tui"
+	"flag"
 	"log"
 
 	tea "github.com/charmbracelet/bubbletea"
 )
 
 func main() {
-	f, err := tea.LogToFile("debug.log", "debug")
-	if err != nil {
-		log.Fatalf("ERRO: %v", err)
+	debug := false
+	flag.BoolVar(&debug, "debug", debug, "Enable debug logging")
+	flag.Parse()
+
+	if debug {
+		f, err := tea.LogToFile("debug.log", "debug")
+		if err != nil {
+			log.Fatalf("ERRO: %v", err)
+		}
+		defer f.Close()
 	}
-	defer f.Close()
+
 
 	p := tea.NewProgram(tui.NewModel(), tea.WithAltScreen())
 	if _, err := p.Run(); err != nil {
